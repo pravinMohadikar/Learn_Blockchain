b5a48955dd36cae91f119634ece0b4ecf3bc456e
pravin <pravin@rapidinnovation.dev>
2023-01-10 13:38:34 +0530

    PYT-44-fix/todo list program

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('@openzeppelin/hardhat-upgrades');
 
+const secrets = require('./secrets');
+
+const ALCHEMY_API_KEY = secrets.ALCHEMY_API_KEY;
+
+const GOERLI_PRIVATE_KEY = secrets.GOERLI_PRIVATE_KEY;
+
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  }
 };
+
